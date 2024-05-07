3afb7bb127007218aebf57ebb98bc179e67890aa
Flychuban <kaloanas07@gmail.com>
2024-05-07 21:56:59 +0300

    Add character encoding for input and output tensors

--- generate_poem.py
+++ generate_poem.py
@@ -24,3 +24,13 @@ next_characters = []
 for i in range(0, len(text_binary) - SEQUENCE_LENGTH, STEP_SIZE):
     sentences.append(text_binary[i: i + SEQUENCE_LENGTH])
     next_characters.append(text_binary[i + SEQUENCE_LENGTH])
+
+# Create the input and output tensors
+x = np.zeros((len(sentences), SEQUENCE_LENGTH, len(characters_dictionary)), dtype=bool)
+y = np.zeros((len(sentences), len(characters_dictionary)), dtype=bool)
+
+# Encode the characters which are present in the text into the tensors x and y
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        x[i, t, char_to_index[char]] = 1
+    y[i, char_to_index[next_characters[i]]] = 1
